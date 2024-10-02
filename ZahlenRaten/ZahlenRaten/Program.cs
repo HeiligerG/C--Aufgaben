9ef4bb0497f84a3533c0b072166f2988677af92a
HolyGBLJ <gggigi111@gmail.com>
2024-10-02 10:34:35 +0200

    Hinzufügen des ZahlenRaten

---
+++ ZahlenRaten/ZahlenRaten/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ZahlenRaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Zahlenprüfer zahlenprüfer = new Zahlenprüfer();
+
+            zahlenprüfer.myrandom = random.Next(1, 101);
+
+            bool spielLäuft = true;
+            while (spielLäuft)
+            {
+                Console.WriteLine("Finde die Zahl zwischen 1 - 100: ");
+                if (int.TryParse(Console.ReadLine(), out int eingabe))
+                {
+                    spielLäuft = !zahlenprüfer.Checker(eingabe);
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
+                }
+            }
+        }
+    }
+}
+
+//Ratespiel zwischen 1 und 100
+
+//Klassen: ZufalsgeneratorXX, Zahlenprüfer, 
---
+++ ZahlenRaten/ZahlenRaten/Zahlenprüfer.cs
@@ -0,0 +1,22 @@
+﻿public class Zahlenprüfer
+    {
+        public int myrandom;
+
+        public bool Checker(int zahl)
+        {
+            if (zahl == myrandom)
+            {
+                Console.WriteLine("Die Zahl stimmt, Gratulation!");
+                return true;
+            }
+            else if (zahl > myrandom)
+            {
+                Console.WriteLine("Die Zahl ist kleiner!");
+            }
+            else
+            {
+                Console.WriteLine("Die Zahl ist grösser!");
+            }
+            return false;
+        }
+    }
