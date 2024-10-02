fd7a2c7ca51c23e6c22b1ef05fd38baffc11245e
HolyGBLJ <gggigi111@gmail.com>
2024-10-02 14:54:49 +0200

    einige verbesserungen und zugaben

--- ZahlenRaten/ZahlenRaten/Program.cs
+++ ZahlenRaten/ZahlenRaten/Program.cs
@@ -7,9 +7,9 @@ namespace ZahlenRaten
         static void Main(string[] args)
         {
             Random random = new Random();
-            Zahlenprüfer zahlenprüfer = new Zahlenprüfer();
+            Zahlenprüfer myzahlenprüfer = new Zahlenprüfer();
 
-            zahlenprüfer.myrandom = random.Next(1, 101);
+            myzahlenprüfer.myrandom = random.Next(1, 101);
 
             bool spielLäuft = true;
             while (spielLäuft)
@@ -17,7 +17,7 @@ namespace ZahlenRaten
                 Console.WriteLine("Finde die Zahl zwischen 1 - 100: ");
                 if (int.TryParse(Console.ReadLine(), out int eingabe))
                 {
-                    spielLäuft = !zahlenprüfer.Checker(eingabe);
+                    spielLäuft = !myzahlenprüfer.Checker(eingabe);
                 }
                 else
                 {
